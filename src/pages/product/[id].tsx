2d59c9ae3ed6cc7a8084306b7a83a7fad7bdc192
José Vitor <jose-vitor-7@hotmail.com>
2024-02-01 16:57:55 -0300

    Product & Checkout - api routes

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -12,14 +12,13 @@ interface ProductProps {
     imageUrl: string
     price: string
     description: string
+    defaultPriceId: string
   }
 }
 
 export default function Product({ product }: ProductProps) {
-  const { isFallback } = useRouter()
-
-  if (isFallback) {
-    return <p>Loading ...</p>
+  function handleBuyProduct(){
+    console.log(product.defaultPriceId)
   }
 
   return (
@@ -33,7 +32,7 @@ export default function Product({ product }: ProductProps) {
 
         <p>{product.description}</p>
 
-        <button>Comprar agora</button>
+        <button onClick={handleBuyProduct}>Comprar agora</button>
       </ProductDetails>
     </ProductContainer>
   )
@@ -71,7 +70,8 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
           style: 'currency',
           currency: 'BRL'
         }).format(price.unit_amount / 100),
-        description: product.description
+        description: product.description,
+        defaultPriceId: price.id
       }
     },
     revalidate: 60 * 60 * 1, // 1 hour
