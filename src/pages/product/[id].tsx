d5365cfd413812b81af76c18fb507db750ea6581
José Vitor <jose-vitor-7@hotmail.com>
2024-02-01 15:27:07 -0300

    Product & Checkout - product page structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,23 @@
 import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
+import Image from "next/image"
 
 export default function Product() {
   const { query } = useRouter()
 
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+        {/* <Image /> */}
+      </ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti tempore non error esse tempora corporis numquam cumque officiis accusamus obcaecati, unde iure! Quis facere quidem repudiandae fuga alias, a quo.</p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
